9f2f64e0156231c37516c6f546b4982e3e3b1b4e
Ravi Rani <ravi@eventbrite.com>
2014-09-06 23:41:28 -0700

    Added view holder pattern and started persisting completion status
    SimpleToDo master

--- app/src/main/java/ravcode/com/mytodoapp/adapters/ListViewAdapter.java
+++ app/src/main/java/ravcode/com/mytodoapp/adapters/ListViewAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,6 +15,13 @@ import java.util.ArrayList;
  */
 public class ListViewAdapter extends ArrayAdapter<ravcode.com.mytodoapp.ToDoItem> {
 
+    private static final String TAG = "ListViewAdapter";
+
+    private static class ViewHolder {
+        TextView itemName;
+        CheckBox itemCompletedCheckbox;
+    }
+
     public ListViewAdapter(Context context, ArrayList<ravcode.com.mytodoapp.ToDoItem> items) {
         super(context, R.layout.list_item_view, items);
     }
@@ -21,13 +29,35 @@ public class ListViewAdapter extends ArrayAdapter<ravcode.com.mytodoapp.ToDoItem
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ravcode.com.mytodoapp.ToDoItem item = getItem(position);
+
+        ViewHolder viewHolder;
+
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_view, parent, false);
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.list_item_view, parent, false);
+            viewHolder.itemName = (TextView)convertView.findViewById(R.id.itemName);
+            viewHolder.itemCompletedCheckbox = (CheckBox)convertView.findViewById(R.id.itemNameCheckbox);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder)convertView.getTag();
         }
 
-        TextView itemName = (TextView)convertView.findViewById(R.id.itemName);
-        itemName.setText(item.text);
+        viewHolder.itemName.setText(item.text);
+        viewHolder.itemCompletedCheckbox.setChecked(item.completed);
+        viewHolder.itemCompletedCheckbox.setTag(item.getId());
+
+        viewHolder.itemCompletedCheckbox.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                CheckBox completedCheckbox = (CheckBox)view;
+                ravcode.com.mytodoapp.ToDoItem toDoItem = ravcode.com.mytodoapp.ToDoItem.getItemById(((Long)completedCheckbox.getTag()).longValue());
+                toDoItem.completed = completedCheckbox.isChecked();
+                toDoItem.save();
+            }
+        });
+
         return convertView;
     }
 }
